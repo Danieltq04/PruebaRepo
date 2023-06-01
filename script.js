f50f651fa1b46fe206b74b0c94c5f61e33a2c40b
Danieltq04 <daniel.tq04@gmail.com>
2023-06-01 00:21:36 -0300

    cambios

--- script.js
+++ script.js
@@ -5,4 +5,21 @@ function toggleMenu() {
     navbar.classList.toggle("open");
     menuIcon.classList.toggle("close");
   }
-  
+
+function incrementarContador() {
+  var visitas = localStorage.getItem("visitas");
+  if (visitas) {
+    visitas = parseInt(visitas) + 1;
+  } else {
+    visitas = 1;
+  }
+  localStorage.setItem("visitas", visitas);
+  document.getElementById("contadorVisitas").textContent = "Visitas: " + visitas;
+}
+
+
+
+window.onload = function() {
+  incrementarContador();
+};
+
